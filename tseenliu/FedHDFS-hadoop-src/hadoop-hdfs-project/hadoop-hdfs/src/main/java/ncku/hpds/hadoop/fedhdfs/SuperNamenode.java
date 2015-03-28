d57b989e73886c8c389f887d51044c600f9174a4
zerostyle0327 <zerostyle0327@gmail.com>
2015-03-29 00:02:26 +0800

    create supernamenode daemon that can get fedfs info periodically

--- tseenliu/FedHDFS-hadoop-src/hadoop-hdfs-project/hadoop-hdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/SuperNamenode.java
+++ tseenliu/FedHDFS-hadoop-src/hadoop-hdfs-project/hadoop-hdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/SuperNamenode.java
@@ -3,6 +3,7 @@ package ncku.hpds.hadoop.fedhdfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.Vector;
 import java.util.concurrent.Callable;
@@ -14,60 +15,94 @@ public class SuperNamenode {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		Thread newThread = new Thread(new TimerThread());
+		newThread.start();
 		
-		String[] uri = args;
-		Configuration[] conf = new Configuration[args.length];
+		/*Thread newThread1 = new Thread(new ReadUserDefinedLT());
+		newThread1.start();*/
 		
-		java.io.File path = new java.io.File("etc/hadoop/fedhadoop-clusters.xml");
-		FedHdfsConParser hdfsIpList = new FedHdfsConParser(path);
-		Vector<Element> theElements = hdfsIpList.getElements();
+		Thread newThread2 = new Thread(new GlobalNamespaceRunnable());
+		newThread2.setDaemon(true);
+		newThread2.start();
 
-		new Thread(new FetchFsimageRunnable()).start();
-		new Thread(new PrintRunnable()).start();
 	}
 }
 
-class PrintRunnable implements Runnable {
+class TimerThread implements Runnable {
 
 	@Override
 	public void run() {
-		final Scanner in = new Scanner(System.in);
-		while (in.hasNext()) {
-			final String line = in.nextLine();
-			System.out.println("Input line: " + line);
-			if ("end".equalsIgnoreCase(line)) {
-				System.out.println("Ending one thread");
-				break;
-			}
-		}
-	}
-
-}
-
-class FetchFsimageRunnable implements Runnable {
-
-	@Override
-	public void run() {
-		int i = 5;
-		while (i > 0) {
-			
-			System.out.println("download loading: " + i--);
-
+		while(true){
 			try {
 				Thread.sleep(1000);
-			} catch (InterruptedException ex) {
-				throw new IllegalStateException(ex);
+				Date now = new Date();
+				System.out.println("Time now : " + now);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
-		System.out.println("!!!! FedHdfsFsImage download sucessful !!!!");
-		
-		/*FetchFsimage.initialize();
-		for (int i = 0; i < args.length; i++) {
-			//FetchFsimage.initialize();
-			FetchFsimage.downloadFedHdfsFsImage(FedHdfsConParser.getValue("HostName", theElements.elementAt(i)), FedHdfsConParser.getValue("dfs.namenode.http-address", theElements.elementAt(i)));
-			FetchFsimage.offlineImageViewer(FedHdfsConParser.getValue("HostName", theElements.elementAt(i))); */
-		
-		
 	}
+}
 
+class ReadUserDefinedLT implements Runnable {
+
+    @Override
+    public void run() {
+    	
+    	GlobalNamespace GN1 = new GlobalNamespace();
+        final Scanner in = new Scanner(System.in);
+        System.out.println("Please input logical mapping : ");
+        System.out.println("(ex : logicalName --> hostName:Path)");
+        while(in.hasNext()) {
+            final String line = in.nextLine();
+            if ("end".equalsIgnoreCase(line)) {
+                System.out.println("Ending one thread");
+                break;
+            }
+            String[] split = line.split(" --> ");
+            System.out.println("Logical File is : " + split[0]);
+            String[] subSplit = split[1].split(":");
+            System.out.println("HostName is : " + subSplit[0]);
+            System.out.println("Physical Path is : " + subSplit[1]);
+            
+            try {
+    			GN1.UserDefinedVD(split[0], subSplit[0], subSplit[1]);
+    		} catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+  
+           
+            System.out.println("Please input logical mapping : ");
+            System.out.println("(ex : logicalName --> hostName:Path)");
+        }
+    }
+
+}
+
+class GlobalNamespaceRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		try{
+			while(true){
+				Thread.sleep(60000);
+				GlobalNamespace GN1 = new GlobalNamespace();
+				GN1.setFedConf();
+				GN1.DynamicConstructPD();
+				
+				System.out.println("!!!! PHashTable download sucessful !!!!");
+				
+				//GN1.createVDrive();
+				
+				//System.out.println("!!!! LHashTable download sucessful !!!!");
+			}
+			
+		}catch(InterruptedException e){	
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
