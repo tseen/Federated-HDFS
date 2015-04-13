cdf252af6f9c04bfe69446658f607ff9bb622f8e
zerostyle0327 <zerostyle0327@gmail.com>
2015-04-13 18:12:08 +0800

    add call-fedHdfs shell's function

--- tseenliu/FedHDFS-hadoop-src/hadoop-hdfs-project/hadoop-hdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/FedHdfs.java
+++ tseenliu/FedHDFS-hadoop-src/hadoop-hdfs-project/hadoop-hdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/FedHdfs.java
@@ -3,10 +3,15 @@ package ncku.hpds.hadoop.fedhdfs;
 import java.util.Vector;
 
 import ncku.hpds.hadoop.fedhdfs.shell.ConstructGN;
+import ncku.hpds.hadoop.fedhdfs.shell.Delete;
+import ncku.hpds.hadoop.fedhdfs.shell.DeleteDir;
 import ncku.hpds.hadoop.fedhdfs.shell.FetchFsimage;
 import ncku.hpds.hadoop.fedhdfs.shell.Ls;
 import ncku.hpds.hadoop.fedhdfs.shell.LsTableInfo;
 import ncku.hpds.hadoop.fedhdfs.shell.Lsr;
+import ncku.hpds.hadoop.fedhdfs.shell.Mkdir;
+import ncku.hpds.hadoop.fedhdfs.shell.Put;
+import ncku.hpds.hadoop.fedhdfs.shell.SecurePut;
 
 import org.apache.hadoop.conf.Configuration;
 import org.w3c.dom.Element;
@@ -27,7 +32,7 @@ public class FedHdfs {
 		for (int i = 0; i < theElements.size(); i++) {
 			conf[i] = new Configuration();
 			conf[i].set(
-					"fs.default.name",
+					"fs.defaultFS",
 					"hdfs://"
 							+ FedHdfsConParser.getValue("fs.default.name",
 									theElements.elementAt(i)));
@@ -38,69 +43,76 @@ public class FedHdfs {
 			if (uri.length < 3) {
 				System.out.println("Usage: hadoop fedfs [generic options]");
 				System.out.println("        [-ls [hostName] [<path> ...]]\n");
-				
-				System.out.println("\nThe general command line syntax is");
-				System.out.println("bin/fedhdfs command [genericOptions] [commandOptions]\n");
 			}
+			
 			for (int i = 0; i < theElements.size(); i++) {
     			if (uri[1].equalsIgnoreCase(FedHdfsConParser.getValue("HostName", theElements.elementAt(i)))) {
     				Ls.print_info(uri[2], conf[i], FedHdfsConParser.getValue("HostName",
     						theElements.elementAt(i)));
     			}
     		}
-		}
-		else if (command.equalsIgnoreCase("-lsr")) {
+			
+		} else if (command.equalsIgnoreCase("-lsr")) {
 			
 			if (uri.length < 3) {
 				System.out.println("Usage: hadoop fedfs [generic options]");
 				System.out.println("        [-ls [hostName] [<path> ...]]\n");
-				
-				System.out.println("\nThe general command line syntax is");
-				System.out.println("bin/fedhdfs command [genericOptions] [commandOptions]\n");
 			}
+			
 			for (int i = 0; i < theElements.size(); i++) {
     			if (uri[1].equalsIgnoreCase(FedHdfsConParser.getValue("HostName", theElements.elementAt(i)))) {
     				Lsr.printFilesRecursively(uri[2], conf[i]);	
     			}
     		}
-		}
-		else if (command.equalsIgnoreCase("-fetchFedImage")){
+			
+		} else if (command.equalsIgnoreCase("-mkdir")){
+			
+			if (uri.length < 2) {
+				System.out.println("Usage: hadoop fedfs [generic options]");
+				System.out.println("        [-gn <logicalName>  <hostName>:<path>]\n");
+			}
+			
+			Mkdir test = new Mkdir();
+			test.logicalMapping(command, uri[1]);	
+			
+		} else if (command.equalsIgnoreCase("-rm")){
+
+			if (uri.length < 3) {
+				System.out.println("Usage: hadoop fedfs [generic options]");
+				System.out.println("        [-gn <logicalName>  <hostName>:<path>]\n");
+			}
+
+			Delete test = new Delete();
+			test.logicalMapping(command, uri[1], uri[2]);
+			
+		} else if (command.equalsIgnoreCase("-rmdir")) {
+
+			if (uri.length < 2) {
+				System.out.println("Usage: hadoop fedfs [generic options]");
+				System.out.println("        [-gn <logicalName>  <hostName>:<path>]\n");
+			}
+
+			DeleteDir test = new DeleteDir();
+			test.logicalMapping(command, uri[1]);
+			
+		} else if (command.equalsIgnoreCase("-lsP")) {
+			
+			LsTableInfo test = new LsTableInfo();
+			//test.GlobalNamespaceClient(uri[1]);
+			
+		} else if (command.equalsIgnoreCase("-fetchFedImage")){
+			
 			FetchFsimage.initialize();
+			
     		for (int i = 0; i < theElements.size(); i++) {
     			FetchFsimage.downloadFedHdfsFsImage(FedHdfsConParser.getValue("HostName", theElements.elementAt(i)), FedHdfsConParser.getValue("dfs.namenode.http-address", theElements.elementAt(i)));
     			//FetchFsimage.offlineImageViewer(FedHdfsConParser.getValue("HostName", theElements.elementAt(i)));
     		}
-		}
-		else if (command.equalsIgnoreCase("-gn")){
-			
-			if (uri.length < 3) {
-				System.out.println("Usage: hadoop fedfs [generic options]");
-				System.out.println("        [-gn <logicalName>  <hostName>:<path>]\n");
-				
-				System.out.println("\nThe general command line syntax is");
-				System.out.println("bin/fedhdfs command [genericOptions] [commandOptions]\n");
-			}
-			
-			ConstructGN test = new ConstructGN();
-			test.logicalMapping(uri[1], uri[2]);	
-		}
-		else if (command.equalsIgnoreCase("-lstable")){
-			
-			if (uri.length < 3) {
-				System.out.println("Usage: hadoop fedfs [generic options]");
-				System.out.println("        [-gn <logicalName>  <hostName>:<path>]\n");
-				
-				System.out.println("\nThe general command line syntax is");
-				System.out.println("bin/fedhdfs command [genericOptions] [commandOptions]\n");
-			}
-			
-			LsTableInfo test = new LsTableInfo();
-			test.GlobalNamespaceClient();
-		}
-		else {
-		
+    		
+		} else {
+
 			System.out.println("The general command line syntax is");
-			System.out.println("bin/fedhdfs command [genericOptions] [commandOptions]\n");
+			System.out.println("bin/fedhdfs command [genericOptions] [commandOptions]");
 		}
 	}
 }
