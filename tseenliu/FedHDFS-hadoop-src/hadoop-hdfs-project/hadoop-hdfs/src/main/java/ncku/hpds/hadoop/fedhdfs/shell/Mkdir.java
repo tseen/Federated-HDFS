516f2d085315776e8317862a0f051c6c8e56d05b
zerostyle0327 <zerostyle0327@gmail.com>
2015-04-13 18:31:57 +0800

    add fedHdfs shell

---
+++ tseenliu/FedHDFS-hadoop-src/hadoop-hdfs-project/hadoop-hdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/shell/Mkdir.java
@@ -0,0 +1,71 @@
+package ncku.hpds.hadoop.fedhdfs.shell;
+
+import java.io.BufferedOutputStream;
+import java.io.ObjectInputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import ncku.hpds.hadoop.fedhdfs.GlobalNamespaceObject;
+
+public class Mkdir {
+	
+	private String SNaddress = "127.0.0.1";
+    private int SNport = 8765;
+    private int port = 8764;
+    
+	public void logicalMapping(String command, String globalFileName) {
+		
+		Socket client = new Socket();
+		ObjectInputStream ObjectIn;
+		InetSocketAddress isa = new InetSocketAddress(this.SNaddress, this.port);
+		try {
+			client.connect(isa, 10000);
+			ObjectIn = new ObjectInputStream(client.getInputStream());
+
+			// received object
+			GlobalNamespaceObject GN = new GlobalNamespaceObject();
+			try {
+				GN = (GlobalNamespaceObject) ObjectIn.readObject();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+			//doing...
+			if (GN.getGlobalNamespace().getLogicalDrive().getLogicalMappingTable().containsKey(globalFileName)) {
+	        	System.out.println("Error: " + globalFileName + " already exists");
+	        }
+	        else {
+	        	Socket SNclient = new Socket();
+	            InetSocketAddress SNisa = new InetSocketAddress(this.SNaddress, this.SNport);
+	            try {
+	            	SNclient.connect(SNisa, 10000);
+	                BufferedOutputStream out = new BufferedOutputStream(SNclient
+	                        .getOutputStream());
+	            // send message
+	    	    String message = command + " " + globalFileName;
+	                out.write(message.getBytes());
+	                out.flush();
+	                out.close();
+	                out = null;
+	                SNclient.close();
+	                SNclient = null;
+	     
+	            } catch (java.io.IOException e) {
+	                System.out.println("Socket connect error");
+	                System.out.println("IOException :" + e.toString());
+	            }
+	        }
+
+			ObjectIn.close();
+			ObjectIn = null;
+			client.close();
+
+		} catch (java.io.IOException e) {
+			System.out.println("Socket connection error");
+			System.out.println("IOException :" + e.toString());
+		}
+
+		System.out.println("globalFileName : " + globalFileName);
+	}
+}
---
+++ tseenliu/FedHDFS-hadoop-src/hadoop-hdfs-project/hadoop-hdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/shell/SecurePut.java
@@ -0,0 +1,42 @@
+package ncku.hpds.hadoop.fedhdfs.shell;
+
+import java.io.BufferedOutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+
+public class SecurePut {
+	
+	private String SNaddress = "127.0.0.1";
+    private int SNport = 8765;
+    
+	public void logicalMapping(String command, String globalFileName, String Path) {
+		
+		String[] split = Path.split(":");
+		String hostName = split[0];
+		String clusterpath = split[1];
+		
+		Socket client = new Socket();
+        InetSocketAddress isa = new InetSocketAddress(this.SNaddress, this.SNport);
+        try {
+            client.connect(isa, 10000);
+            BufferedOutputStream out = new BufferedOutputStream(client
+                    .getOutputStream());
+       
+	    String message = command + " " + globalFileName + " " + hostName + " " + clusterpath;
+            out.write(message.getBytes());
+            out.flush();
+            out.close();
+            out = null;
+            client.close();
+            client = null;
+ 
+        } catch (java.io.IOException e) {
+            System.out.println("Socket connect error");
+            System.out.println("IOException :" + e.toString());
+        }
+	    
+		 System.out.println("globalFileName : " + globalFileName);
+	     System.out.println("host and path : " + Path );
+	}
+}
