109cd0a261d3c72e37132882a3f0ae8025a282c1
zerostyle0327 <zerostyle0327@gmail.com>
2015-04-19 13:43:36 +0800

    rename LsTableInfo -> LsGlobalNamespace

---
+++ tseenliu/FedHDFS-hadoop-src/hadoop-hdfs-project/hadoop-hdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/shell/LsGlobalNamespace.java
@@ -0,0 +1,109 @@
+package ncku.hpds.hadoop.fedhdfs.shell;
+
+import java.io.ObjectInputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.URI;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import ncku.hpds.hadoop.fedhdfs.GlobalNamespaceObject;
+
+public class LsTableInfo {
+
+	private String address = "127.0.0.1";
+	private int port = 8764;
+	
+	public void GlobalNamespaceClient() {
+		
+		String Uri = "/";
+		Socket client = new Socket();
+		ObjectInputStream ObjectIn;
+		InetSocketAddress isa = new InetSocketAddress(this.address, this.port);
+		try {
+			client.connect(isa, 10000);
+			ObjectIn = new ObjectInputStream(client.getInputStream());
+			
+			//received object
+			GlobalNamespaceObject GN = new GlobalNamespaceObject();
+			try {
+				GN = (GlobalNamespaceObject) ObjectIn.readObject();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("physicalDrive : " + GN.getGlobalNamespace().getLogicalDrive().getLogicalHashTable());
+			
+			
+			/*for (int i = 0; i < GN.getGlobalNamespace().getLogicalDrive().getLogicalMappingTable().get(Uri).size(); i++) {
+				if ("VirtualDrive".equalsIgnoreCase(GN.getGlobalNamespace().getLogicalDrive().getLogicalMappingTable().get(Uri).)) {
+					show.DirFileInfo(status[i].getPath().toString(), conf);
+					printFilesRecursively(status[i].getPath().toString(), conf);
+
+				} else {
+					try {
+						show.DirFileInfo(status[i].getPath().toString(), conf);
+					} catch (Exception e) {
+						System.err.println(e.toString());
+					}
+				}
+			}*/
+			
+			
+			System.out.println("fedUserGet2 : " + GN.getGlobalNamespace().getPhysicalDrive().getPhysicalMappingTable().size());
+			System.out.println("fedUserGet3 : " + GN.showLogicalMapping());
+			
+			/*System.out.println(GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get("FedRDD").get("c34").get(1)
+					+ String.format("%4d", GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get("FedRDD").get("c34").get(2))
+					+ " "
+					+ GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get("FedRDD").get("c34").get(3)
+					+ " "
+					+ GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get("FedRDD").get("c34").get(4)
+					+ String.format("%11d", GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get("FedRDD").get("c34").get(5))
+					+ " "
+					+ GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get("FedRDD").get("c34").get(6)
+					+ " " + GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get("FedRDD").get("c34").get(0));
+			
+			int VirtualSize = GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().size();
+			Set<String> keySet = GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().keySet();
+			Iterator<String> it = keySet.iterator();
+			while(it.hasNext()){
+				String key = it.next();
+				LsTableInfo.showInfo(GN, key);
+			}
+			for (int i = 0; i < VirtualSize ; i++) {
+				GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable();
+			}*/
+			
+			ObjectIn.close();
+			ObjectIn = null;
+			client.close();
+			
+		} catch (java.io.IOException e) {
+			System.out.println("Socket connection error");
+			System.out.println("IOException :" + e.toString());
+		}
+	}
+	
+	/*public static void showInfo(GlobalNamespaceObject GN, String key) {
+		
+		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		System.out.println(GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get(key).get("c34").get(1)
+				+ String.format("%4d", GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get(key).get("c34").get(2))
+				+ " "
+				+ GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get(key).get("c34").get(3)
+				+ " "
+				+ GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get(key).get("c34").get(4)
+				+ String.format("%11d", GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get(key).get("c34").get(5))
+				+ " "
+				+ GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get(key).get("c34").get(6)
+				+ " " + GN.getGlobalNamespace().getLogicalDrive().TestgetLogicalMappingTable().get(key).get("c34").get(0));
+	}*/
+	
+}
