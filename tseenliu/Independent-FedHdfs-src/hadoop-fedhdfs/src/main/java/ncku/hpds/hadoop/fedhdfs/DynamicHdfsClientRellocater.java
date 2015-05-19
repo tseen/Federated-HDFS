9f8470e074ceafbc30a2994606faefa977d166c6
zerostyle0327 <zerostyle0327@gmail.com>
2015-05-19 12:55:19 +0800

    Try and Error.

--- tseenliu/Independent-FedHdfs-src/hadoop-fedhdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/DynamicHdfsClientRellocater.java
+++ tseenliu/Independent-FedHdfs-src/hadoop-fedhdfs/src/main/java/ncku/hpds/hadoop/fedhdfs/DynamicHdfsClientRellocater.java
@@ -1,32 +1,25 @@
 package ncku.hpds.hadoop.fedhdfs;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Vector;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.hadoop.fs.FsStatus;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.util.StringUtils;
+import org.w3c.dom.Element;
 
 public class DynamicHdfsClientRellocater {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
-		
-		
-		//Configuration conf = new HdfsConfiguration();
-		DistributedFileSystem cluster1 = new DistributedFileSystem();
-		
-		System.out.println(cluster1.getConf().toString());
-		
+		String Alex = args[0];
+		File XMfile = new File("etc/hadoop/fedhadoop-clusters.xml"); 
+		TopcloudSelector top = new TopcloudSelector(XMfile, Alex);
+		System.out.println("ANS :" + top.getTopCloud());
 	}
 }
