1b67559b8f3df9e41747036a4af2fe955dd3c902
Chun-Yu Wang <wicanr2@gmail.com>
2016-09-29 00:41:08 +0800

    merge fedhdfs into hadoop-client-core for maven building

---
+++ TzuEn/hadoop-mapreduce-client-core-2.7.2/src/main/java/ncku/hpds/hadoop/fedhdfs/SuperNamenodeInfo.java
@@ -0,0 +1,47 @@
+package ncku.hpds.hadoop.fedhdfs;
+
+
+public class SuperNamenodeInfo {
+
+	public static String Default_SuperNamenodeAddress = "127.0.0.1";
+	public static String Default_FedUserConstructGNPort = "8765";
+	public static String Default_GlobalNamespaceServerPort = "8764";
+	public static String Default_GNQueryServerPort = "8763";
+	public static String Default_Alpha = "1.0";
+
+	public void setSuperNamenodeAddress(String SuperNamenodeAddress) { this.Default_SuperNamenodeAddress = SuperNamenodeAddress; }
+	public void setFedUserConstructGNPort(String FedUserConstructGNPort) { this.Default_FedUserConstructGNPort = FedUserConstructGNPort; }
+	public void setGlobalNamespaceServerPort(String GlobalNamespaceServerPort) { this.Default_GlobalNamespaceServerPort = GlobalNamespaceServerPort; }
+	public void setGNQueryServerPort(String GNQueryServerPort) { this.Default_GNQueryServerPort = GNQueryServerPort; }
+	public void setAlpha(String Alpha) { this.Default_Alpha = Alpha; }
+	
+	
+	public static String getSuperNamenodeAddress() { return Default_SuperNamenodeAddress; }
+	
+	public static int getFedUserConstructGNPort() {
+		return Integer.parseInt(Default_FedUserConstructGNPort);
+	}
+	
+	public static int getGlobalNamespaceServerPort() { 
+		return Integer.parseInt(Default_GlobalNamespaceServerPort);
+	}
+	
+	public static int getGNQueryServerPort() {
+		return Integer.parseInt(Default_GNQueryServerPort);
+	}
+	
+	public static double getAlpha() {
+		return Double.parseDouble(Default_Alpha);
+	}
+	
+	
+	
+	public static void show() {
+		System.out.println(Default_SuperNamenodeAddress);
+		System.out.println(Default_FedUserConstructGNPort);
+		System.out.println(Default_GlobalNamespaceServerPort);
+		System.out.println(Default_GNQueryServerPort);
+		System.out.println(Default_Alpha);
+	}
+
+}
