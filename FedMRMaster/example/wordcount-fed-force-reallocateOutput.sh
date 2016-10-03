4b52579635f748278fb55a6526c2aa0f3df9f378
zerostyle0327 <zerostyle0327@gmail.com>
2016-10-03 22:44:29 +0800

    refresh

--- FedMRMaster/example/wordcount-fed-force-reallocateOutput.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.2.jar wordcount -Dfed=true -DfedHdfs=true -DwanOpt=false -Dmain=wordcount -DtopNumbers=1 -DArg0=test_input2 -DArg1=fed_output_reallocate test_input2 fed_output_reallocate 
-
--- FedMRMaster/example/wordcount-fed.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.2.jar wordcount -Dfed=true -DfedHdfs=true -Dmain=wordcount -DArg0=test_input2 -DArg1=fed_output7 test_input2 fed_output7
-
